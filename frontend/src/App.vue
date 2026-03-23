50410d4096a405fc604bc449ee61471901a9c5e0
Parvinder <techpinda1214@gmail.com>
2026-03-23 22:38:37 +0530

    Initial Setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-shell">
+    <AppNav v-if="auth.isLoggedIn" />
+    <main class="app-main">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { RouterView } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
+import AppNav from '@/components/shared/AppNav.vue'
+
+const auth = useAuthStore()
+
+onMounted(() => {
+  auth.fetchMe()
+})
+</script>
